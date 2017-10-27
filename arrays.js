3655c283d170489e4300d0f92e95c17667c0232c
Awtian Akbar <me@awtian.com>
2017-10-27 14:29:14 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -10,7 +10,7 @@ function addElementToBeginningOfArray (array, element) {
 };
 
 function destructivelyAddElementToBeginningOfArray (array, element) {
-  return array.unshift(element)
+  array.unshift(element)
 };
 
 function addElementToEndOfArray (array, element) {

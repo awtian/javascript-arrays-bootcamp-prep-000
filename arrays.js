e66ddd5acb0620d467d13f115a37c893affaa8c0
Awtian Akbar <me@awtian.com>
2017-10-27 15:14:43 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -28,7 +28,7 @@ function accessElementInArray (array, index) {
 };
 
 function destructivelyRemoveElementFromBeginningOfArray (array) {
-   array.pop()
+   array.shift()
    return array
 }
 function removeElementFromBeginningOfArray (array) {

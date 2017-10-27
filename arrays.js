4b7ca8433ecc9bb56a85a5c4debe63c0b1ef9a89
Awtian Akbar <me@awtian.com>
2017-10-27 14:35:56 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -10,7 +10,7 @@ function addElementToBeginningOfArray (array, element) {
 };
 
 function destructivelyAddElementToBeginningOfArray (array, element) {
-  var array = array.unshift(element)
+  return = array.unshift(element)
 };
 
 function addElementToEndOfArray (array, element) {
